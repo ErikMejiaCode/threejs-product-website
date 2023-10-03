c46c9d25a9f4e442a65a74e83c6386fd1509b8d1
Erik Mejia <erikdojo352@gmail.com>
2023-10-03 10:26:32 -0400

    file picker

--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -2,6 +2,8 @@ import { useSnapshot } from "valtio";
 
 import { state } from "../store/index";
 
+import { getContrastingColor } from "../config/helpers";
+
 export const CustomButton = ({ type, title, customStyles, handleClick }) => {
   const snap = useSnapshot(state);
 
@@ -9,14 +11,20 @@ export const CustomButton = ({ type, title, customStyles, handleClick }) => {
     if (type === "filled") {
       return {
         backgroundColor: snap.color,
-        color: "#000",
+        color: getContrastingColor(snap.color),
+      };
+    } else if (type === "outline") {
+      return {
+        borderWidth: "1px",
+        borderColor: snap.color,
+        color: snap.color,
       };
     }
   };
 
   return (
     <button
-      className={`px-2 py-1.2 flex-1 rounded-md ${customStyles}`}
+      className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
       style={generateStyle(type)}
       onClick={handleClick}
     >
--- src/components/FilePicker.jsx
+++ src/components/FilePicker.jsx
@@ -1,5 +1,38 @@
-import React from "react";
+import { CustomButton } from "./CustomButton";
 
-export const FilePicker = () => {
-  return <div>FilePicker</div>;
+export const FilePicker = ({ file, setFile, readFile }) => {
+  return (
+    <div className="filepicker-container">
+      <div className="flex-1 flex flex-col">
+        <input
+          id="file-upload"
+          type="file"
+          accept="image/*"
+          onChange={(e) => setFile(e.target.files[0])}
+        />
+        <label htmlFor="file-upload" className="filepicker-label">
+          Upload FIle
+        </label>
+
+        <p className="mt-2 text-gray-100 text-xs truncate">
+          {file === "" ? "No File Selected" : file.name}
+        </p>
+      </div>
+
+      <div className="mt-4 flex flex-wrap gap-3">
+        <CustomButton
+          type="outline"
+          title="Logo"
+          handleClick={() => readFile("logo")}
+          customStyles="text-xs text-gray-100"
+        />
+        <CustomButton
+          type="filled"
+          title="Full"
+          handleClick={() => readFile("full")}
+          customStyles="text-xs text-gray-100"
+        />
+      </div>
+    </div>
+  );
 };
